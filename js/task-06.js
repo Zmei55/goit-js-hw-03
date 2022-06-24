3d07366242d7e8a8f74e76898a0927c334dd311f
Andrei <andrei.lerke@gmail.com>
2022-06-24 23:27:17 +0200

    update

--- js/task-06.js
+++ js/task-06.js
@@ -1,9 +1,4 @@
-const products = [
-  { name: 'Радар', price: 1300, quantity: 4 },
-  { name: 'Сканер', price: 2700, quantity: 3 },
-  { name: 'Дроид', price: 400, quantity: 7 },
-  { name: 'Захват', price: 1200, quantity: 2 },
-];
+import products from './products.js';
 
 const calculateTotalPrice = (allProdcuts, productName) => {
   let totalPrice = 0;
